07a678d360c325de2441dd73f3a904ac607a181c
firenza <chad.oftedahl@sleepnumber.com>
2021-12-08 14:02:57 -0800

    Remove try block

--- get_dsv.js
+++ get_dsv.js
@@ -19,35 +19,31 @@ let wait = async function (dsv_tenant, dsv_user, dsv_password, dsv_path) {
   });
 
   (async () => {
-    try {
-      await pipeline(downloadStream, fileWriterStream);
-      core.debug(`File downloaded to ${fileName}`);
-      const { exec } = require("child_process");
-      exec(`${fileName} secret read -t "${dsv_tenant}" "${dsv_path}" -u "${dsv_user}" -p "${dsv_password}" -f .data`, (error, stdout, stderr) => {
-        if (error) {
-          core.error(error.message);
-          return;
-        }
-        if (stderr) {
-          core.error(stderr);
-          return;
-        }
-        core.info(stdout);
+    await pipeline(downloadStream, fileWriterStream);
+    core.debug(`File downloaded to ${fileName}`);
+    const { exec } = require("child_process");
+    exec(`${fileName} secret read -t "${dsv_tenant}" "${dsv_path}" -u "${dsv_user}" -p "${dsv_password}" -f .data`, (error, stdout, stderr) => {
+      if (error) {
+        core.error(error.message);
+        return;
+      }
+      if (stderr) {
+        core.error(stderr);
+        return;
+      }
+      core.info(stdout);
 
-        // Parse JSON payload from the dsv cli output
-        const secrets = JSON.parse(stdout);
+      // Parse JSON payload from the dsv cli output
+      const secrets = JSON.parse(stdout);
 
-        // Mark string values as secret and obfuscate in console if displayed
-        // Set action outputs
-        for (var attributeName in secrets) {
-          core.setSecret(secrets[attributeName])
+      // Mark string values as secret and obfuscate in console if displayed
+      // Set action outputs
+      for (var attributeName in secrets) {
+        core.setSecret(secrets[attributeName])
 
-          core.setOutput(attributeName, secrets[attributeName])
-        }
-      });
-    } catch (error) {
-      core.error(error.message);
-    }
+        core.setOutput(attributeName, secrets[attributeName])
+      }
+    });
   })();
 };
 
